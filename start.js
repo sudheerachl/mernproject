aa269be35cd29711946bcad8bdfed4785c75a3ef
sudheerachl <135159697+sudheerachl@users.noreply.github.com>
2023-11-10 08:29:12 +0530

    Update start.js

--- start.js
+++ start.js
@@ -1,4 +1,4 @@
-const { exec } = require('child_process');
+/*const { exec } = require('child_process');
 
 // Command to start backend
 const backendCommand = 'npm start --prefix backend';
@@ -20,4 +20,24 @@ process.stderr.on('data', (data) => {
 process.on('close', (code) => {
   console.log(`Processes exited with code ${code}`);
   // You can add additional handling if needed
+});*/
+const { spawn } = require('child_process');
+
+const startBackend = spawn('node', ['backend/index.js'], {
+  stdio: 'inherit',
+});
+
+// Wait for the backend to start before starting the frontend
+startBackend.on('ready', () => {
+  spawn('npm', ['run', 'dev'], {
+    stdio: 'inherit',
+    cwd: 'frontend',
+  });
+});
+
+startBackend.on('close', (code) => {
+  if (code !== 0) {
+    console.error('Backend exited with code:', code);
+    process.exit(code);
+  }
 });
